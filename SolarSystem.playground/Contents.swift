1184466416201d8db7e0edcb5f1102e06d1d8523
Rohin Tangirala <rohinkt@gmail.com>
2019-07-15 21:40:52 -0700

    initial commit

---
+++ SolarSystem.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: A SpriteKit based Playground
+
+import PlaygroundSupport
+import SpriteKit
+
+// Load the SKScene from 'GameScene.sks'
+let sceneView = SKView(frame: CGRect(x:0 , y:0, width: 600, height: 500))
+
+if let scene = SolarSystemScene(fileNamed: "SolarSystemScene") {
+  // Set the scale mode to scale to fit the window
+  scene.scaleMode = .aspectFill
+    
+  // Present the scene
+  sceneView.presentScene(scene)
+}
+
+PlaygroundSupport.PlaygroundPage.current.liveView = sceneView
+
